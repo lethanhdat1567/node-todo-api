29f189bc4d12400f6b9863881e31801a10b1e5b1
Le Thanh Dat <lethanhdat1567@gmail.com>
2025-12-24 22:21:40 +0700

    fix: use process.env.PORT for deploy

--- server.js
+++ server.js
@@ -170,4 +170,8 @@ const server = createServer((req, res) => {
     }
 });
 
-server.listen(3000, () => console.log("server is running on port 3000"));
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
